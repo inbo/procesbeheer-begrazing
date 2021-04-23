f8514a90cd95f1f77569f95bd94bfe1410fd3da6
hansvancalster <hans.vancalster@inbo.be>
2021-04-23 17:54:52 +0200

    verkenning inlezen data en berekening CHM (WIP)

---
+++ src/utils/utils.R
@@ -0,0 +1,20 @@
+get_map_procesbeheer <- function() {
+  if (Sys.getenv("MAP_PROCESBEHEER") == "") {
+    usethis::edit_r_environ("project")
+    stop(paste0("No MAP_PROCESBEHEER environmental variable found, ",
+                "please edit the .Renviron file and add a line ",
+                "MAP_PROCESBEHEER = '<path to the team drive procesbeheer folder>'"))
+  }
+  return(Sys.getenv("MAP_PROCESBEHEER"))
+}
+
+
+get_map_inbogis <- function() {
+  if (Sys.getenv("MAP_INBOGIS") == "") {
+    usethis::edit_r_environ("project")
+    stop(paste0("No MAP_INBOGIS environmental variable found, ",
+                "please edit the .Renviron file and add a line ",
+                "MAP_INBOGIS = '<path to the team drive procesbeheer folder>'"))
+  }
+  return(Sys.getenv("MAP_INBOGIS"))
+}
